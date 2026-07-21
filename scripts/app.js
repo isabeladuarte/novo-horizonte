516358a24380f84aadce40e7c464a73151fd6476
evypersonal <evellynmaria2015@outlook.com>
2026-07-21 19:10:12 -0300

    test: testando tailly como forms

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,12 @@ const links = document.querySelector('.navbar-links');
 menu.addEventListener('click', () => {
   links.classList.toggle('active');
 });
+
+
+// // Adicionando ação ao botão para formulário
+// document.getElementById('app-btn-primary').addEventListener('click', function(event) {
+//   event.preventDefault();
+//   // Abrir o link do formulário na mesma aba
+//   window.location.href = "https://tally.so/r/GxjJle";
+//   console.log('Botão clicado!');
+// });
